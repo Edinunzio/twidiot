dc19e388e48868c7d4410ab51801216bf973b74d
Edinunzio <e.dinunzio@gmail.com>
2016-06-29 03:24:42 -0400

    adding tests for retweeters list,
    added method to get users original tweets by handle

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,17 +1,33 @@
 import unittest
-from users.user import get_users_by_liked_tweet
+from users.user import get_users_by_liked_tweet, get_users_by_retweets, get_users_tweets
 
 
 class TestUser(unittest.TestCase):
 
     def setUp(self):
+        self.get_users_tweets = get_users_tweets('@Claudie4')
+
         self.get_users = get_users_by_liked_tweet('745892215969423360')
         self.get_false_id = get_users_by_liked_tweet('745892215969423')
         self.get_type_error = get_users_by_liked_tweet(74589221596942)
         self.expected_list = [u'@Claudie4', u'@FancyFreeSydney', u'@GGG_says', u'@renzocapetti29', u'@daniellstevens', u'@TenorioPraxGab', u'@mr_preface', u'@ncartwright34', u'@hrlaube', u'@OkemwaNeaman', u'@JohnnySoftware', u'@momo24k5', u'@davemoskowitz10', u'@SidSeven777']
 
-    def test_get_users_1(self):
+        self.get_r_users = get_users_by_retweets('745892215969423360')
+        self.get_r_false_id = get_users_by_retweets('745892215969423')
+        self.get_r_type_error = get_users_by_retweets(74589221596942)
+        self.r_expected_list = [u'@Claudie4', u'@GGG_says', u'@JasonBerger1', u'@cunningrabbit', u'@QxNews', u'@prattreport', u'@davemoskowitz10', u'@GenXFiles']
+
+    def test_get_users_by_liked_tweet(self):
         self.assertEqual(list, type(self.get_users))
         self.assertEqual(self.expected_list, self.get_users)
         self.assertEqual('Tweet Not Found', self.get_false_id)
         self.assertRaises(TypeError, self.get_type_error)
+
+    def test_get_users_by_retweets(self):
+        self.assertEqual(list, type(self.get_r_users))
+        self.assertEqual(self.r_expected_list, self.get_r_users)
+        self.assertEqual('Tweet Not Found', self.get_r_false_id)
+        self.assertRaises(TypeError, self.get_r_type_error)
+
+    def test_get_users_tweets(self):
+        self.assertEqual(list, type(self.get_users_tweets.status_code))
--- users/user.py
+++ users/user.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-FAVORITED_URL = 'https://twitter.com/i/activity/favorited_popup?id='
+PREFIX = 'https://twitter.com/'
+FAVORITED_URL = PREFIX + 'i/activity/favorited_popup?id='
+RETWEETED_URL = PREFIX + '/i/activity/retweeted_popup?id='
+USER_ACTIVITY = '/with_replies'
+
 
 def get_users_by_liked_tweet(tweet_id):
     try:
@@ -22,3 +26,42 @@ def get_users_by_liked_tweet(tweet_id):
     else:
         return 'An unknown error occured'
 
+
+def get_users_by_retweets(tweet_id):
+    try:
+        url = RETWEETED_URL + tweet_id
+    except TypeError:
+        url = RETWEETED_URL + str(tweet_id)
+
+    r = requests.get(url)
+    if r.status_code == 200:
+        parsed_json = r.json()
+        html = parsed_json['htmlUsers'].encode('ascii', 'ignore')
+        soup = BeautifulSoup(html, "html.parser")
+        usernames = soup.select('.username')
+        retweeted_by = [u.get_text() for u in usernames]
+        return retweeted_by
+    elif r.status_code == 404:
+        return 'Tweet Not Found'
+    else:
+        return 'An unknown error occured'
+
+def get_users_tweets(handle):
+    try:
+        url = PREFIX + handle[1:] + USER_ACTIVITY
+    except TypeError:
+        url = PREFIX + str(handle[1:]) + USER_ACTIVITY
+    r = requests.get(url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        stream = soup.select('.stream-items')[0]
+        li = stream.select('li.stream-item')
+        contents = [l.contents[1] for l in li]
+        original_tweets = []
+        for c in contents:
+            if c['data-screen-name'] == handle[1:]:
+                txt = c.text.split(handle[1:])
+                original_tweets.append(txt[1])
+        return original_tweets
+
+x = get_users_tweets('@DonTrumpsToupee')
