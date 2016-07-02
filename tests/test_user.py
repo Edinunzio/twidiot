e5aed300c9e8df562951b2138bd116e485b48cfb
Edinunzio <e.dinunzio@gmail.com>
2016-07-01 21:51:54 -0400

    now down with OOP,
    yeah, you know me!

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,37 +1,38 @@
 import unittest
-from users.user import get_users_by_liked_tweet, get_users_by_retweets, get_users_tweets
+from users.user import Twidiot
 
 
 class TestUser(unittest.TestCase):
 
     def setUp(self):
-        self.get_users_tweets = get_users_tweets('@Claudie4')
-        self.get_fake_user = get_users_tweets('@ssadasihnlkhdoisyfoih')
-        self.get_user_type_error = get_users_tweets('dfh aja')
+        self.t = Twidiot()
+        self.get_recent_activity = self.t.get_recent_activity('@Claudie4')
+        self.u_not_found = self.t.get_recent_activity('@ssadasihnlkhdoisyfoih')
+        self.get_user_type_error = self.t.get_recent_activity('dfh aja')
 
-        self.get_users = get_users_by_liked_tweet('745892215969423360')
-        self.get_false_id = get_users_by_liked_tweet('745892215969423')
-        self.get_type_error = get_users_by_liked_tweet(74589221596942)
+        self.liked_by = self.t.liked_by('745892215969423360')
+        self.t_not_found = self.t.liked_by('745892215969423')
+        self.get_type_error = self.t.liked_by(74589221596942)
         self.expected_list = [u'@Claudie4', u'@FancyFreeSydney', u'@GGG_says', u'@renzocapetti29', u'@daniellstevens', u'@TenorioPraxGab', u'@mr_preface', u'@ncartwright34', u'@hrlaube', u'@OkemwaNeaman', u'@JohnnySoftware', u'@momo24k5', u'@davemoskowitz10', u'@SidSeven777']
 
-        self.get_r_users = get_users_by_retweets('745892215969423360')
-        self.get_r_false_id = get_users_by_retweets('745892215969423')
-        self.get_r_type_error = get_users_by_retweets(74589221596942)
+        self.get_r_users = self.t.retweeted_by('745892215969423360')
+        self.get_r_false_id = self.t.retweeted_by('745892215969423')
+        self.get_r_type_error = self.t.retweeted_by(74589221596942)
         self.r_expected_list = [u'@Claudie4', u'@GGG_says', u'@JasonBerger1', u'@cunningrabbit', u'@QxNews', u'@prattreport', u'@davemoskowitz10', u'@GenXFiles']
 
-    def test_get_users_by_liked_tweet(self):
-        self.assertEqual(list, type(self.get_users))
-        self.assertEqual(self.expected_list, self.get_users)
-        self.assertEqual('Tweet Not Found', self.get_false_id)
+    def test_get_recent_activity(self):
+        self.assertEqual(list, type(self.get_recent_activity))
+        self.assertEqual("Page Not Found", self.u_not_found)
+        self.assertRaises(TypeError, self.get_user_type_error)
+
+    def test_liked_by(self):
+        self.assertEqual(list, type(self.liked_by))
+        self.assertEqual(self.expected_list, self.liked_by)
+        self.assertEqual('Tweet Not Found', self.t_not_found)
         self.assertRaises(TypeError, self.get_type_error)
 
-    def test_get_users_by_retweets(self):
+    def test_retweeted_by(self):
         self.assertEqual(list, type(self.get_r_users))
         self.assertEqual(self.r_expected_list, self.get_r_users)
         self.assertEqual('Tweet Not Found', self.get_r_false_id)
         self.assertRaises(TypeError, self.get_r_type_error)
-
-    def test_get_users_tweets(self):
-        self.assertEqual(list, type(self.get_users_tweets))
-        self.assertEqual("Page Not Found", self.get_fake_user)
-        self.assertRaises(TypeError, self.get_user_type_error)
--- users/user.py
+++ users/user.py
@@ -7,63 +7,70 @@ RETWEETED_URL = PREFIX + '/i/activity/retweeted_popup?id='
 USER_ACTIVITY = '/with_replies'
 
 
-def get_users_by_liked_tweet(tweet_id):
-    try:
-        url = FAVORITED_URL + tweet_id
-    except TypeError:
-        url = FAVORITED_URL + str(tweet_id)
+class Twidiot(object):
 
-    r = requests.get(url)
-    if r.status_code == 200:
-        parsed_json = r.json()
-        html = parsed_json['htmlUsers'].encode('ascii', 'ignore')
-        soup = BeautifulSoup(html, "html.parser")
-        usernames = soup.select('.username')
-        liked_by = [u.get_text() for u in usernames]
-        return liked_by
-    elif r.status_code == 404:
-        return 'Tweet Not Found'
-    else:
-        return 'An unknown error occured'
+    def __init__(self):
+        self.PREFIX = 'https://twitter.com/'
+        self.FAVORITED_URL = self.PREFIX + 'i/activity/favorited_popup?id='
+        self.RETWEETED_URL = self.PREFIX + '/i/activity/retweeted_popup?id='
+        self.USER_ACTIVITY = '/with_replies'
 
+    def get_recent_activity(self, handle):
+        try:
+            url = self.PREFIX + handle[1:] + self.USER_ACTIVITY
+        except TypeError:
+            url = self.PREFIX + str(handle[1:]) + self.USER_ACTIVITY
+        r = requests.get(url)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            stream = soup.select('.stream-items')[0]
+            li = stream.select('li.stream-item')
+            contents = [l.contents[1] for l in li]
+            tweets = []
+            for c in contents:
+                if c['data-screen-name'] == handle[1:]:
+                    txt = c.text.split(handle[1:])
+                    tweets.append(txt[1])
+            return tweets
+        elif r.status_code == 404:
+            return 'Page Not Found'
+        else:
+            return 'An unknown error occured'
 
-def get_users_by_retweets(tweet_id):
-    try:
-        url = RETWEETED_URL + tweet_id
-    except TypeError:
-        url = RETWEETED_URL + str(tweet_id)
+    def liked_by(self, tweet_id):
+        try:
+            url = self.FAVORITED_URL + tweet_id
+        except TypeError:
+            url = self.FAVORITED_URL + str(tweet_id)
 
-    r = requests.get(url)
-    if r.status_code == 200:
-        parsed_json = r.json()
-        html = parsed_json['htmlUsers'].encode('ascii', 'ignore')
-        soup = BeautifulSoup(html, "html.parser")
-        usernames = soup.select('.username')
-        retweeted_by = [u.get_text() for u in usernames]
-        return retweeted_by
-    elif r.status_code == 404:
-        return 'Tweet Not Found'
-    else:
-        return 'An unknown error occured'
+        r = requests.get(url)
+        if r.status_code == 200:
+            parsed_json = r.json()
+            html = parsed_json['htmlUsers'].encode('ascii', 'ignore')
+            soup = BeautifulSoup(html, "html.parser")
+            usernames = soup.select('.username')
+            liked_by = [u.get_text() for u in usernames]
+            return liked_by
+        elif r.status_code == 404:
+            return 'Tweet Not Found'
+        else:
+            return 'An unknown error occured'
 
-def get_users_tweets(handle):
-    try:
-        url = PREFIX + handle[1:] + USER_ACTIVITY
-    except TypeError:
-        url = PREFIX + str(handle[1:]) + USER_ACTIVITY
-    r = requests.get(url)
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        stream = soup.select('.stream-items')[0]
-        li = stream.select('li.stream-item')
-        contents = [l.contents[1] for l in li]
-        tweets = []
-        for c in contents:
-            if c['data-screen-name'] == handle[1:]:
-                txt = c.text.split(handle[1:])
-                tweets.append(txt[1])
-        return tweets
-    elif r.status_code == 404:
-        return 'Page Not Found'
-    else:
-        return 'An unknown error occured'
+    def retweeted_by(self, tweet_id):
+        try:
+            url = RETWEETED_URL + tweet_id
+        except TypeError:
+            url = RETWEETED_URL + str(tweet_id)
+
+        r = requests.get(url)
+        if r.status_code == 200:
+            parsed_json = r.json()
+            html = parsed_json['htmlUsers'].encode('ascii', 'ignore')
+            soup = BeautifulSoup(html, "html.parser")
+            usernames = soup.select('.username')
+            retweeted_by = [u.get_text() for u in usernames]
+            return retweeted_by
+        elif r.status_code == 404:
+            return 'Tweet Not Found'
+        else:
+            return 'An unknown error occured'
